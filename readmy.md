6fc4d3b912e6ff2e52718b815abfd36c3bc29e19
Saulius Anusas <saulius.anusas@gmail.com>
2023-10-19 21:15:35 +0300

    New task for homework

---
+++ readmy.md
@@ -0,0 +1,11 @@
+Task nr.2:
+Create a program , which takes 3 differnt sentences. 
+The input should have all error checking in mind.
+The program then should create a dictionary of whith key values corresponding to words:
+ - `long` (more than 9 letters in a words)
+ - `medium`(7 letters)
+ - `short` (5 words).
+ Then the pgrogram should create a new sentences (if 3 provided, 3 new sentences should be returned) with following rules attached:
+  - All sentences should have same (or less) words amount as entered one
+  - The most frequent letter from the sentence (from input) should be dominated in a new sentence as well.
+  - The program should return new sentences with statitstics of ratio how many words was used from all sections(as    for exmpale: long 25%,medium 45%, short 30%)
--- task1.py
+++ task1.py
@@ -1,14 +1,11 @@
-from typing import Optional, List
+from typing import List
 
-simbol = input("Please enter string with spec simbol: ")
+simbol = input("Please enter words with spec simbol: ")
 
-def pick_spec_simb(in_string: str) -> Optional(str):
-    spec_simb = ""
-    chars: List[str] = ["!","@","#","$","%","^","&","*","_","+","-"]
-    for simb in in_string:
-        for char in chars:
-            if simb == char:
-                spec_simb: Optional(str) = str(spec_simb) + str(simb)
-    return spec_simb
+def find_word(in_text: str) -> List[str]:
+    unique_chars: List[str] = ["!","@","#","$","%","^","&","*","_","+","-"]
+    splited_words: List[str] = in_text.split(" ")
+    unique_words: List[str] = [word for word in splited_words if any(simb in word for simb in unique_chars)]
+    return unique_words
 
-print(pick_spec_simb(simbol))
+print(find_word(simbol))
